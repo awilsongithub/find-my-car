4aafb933b30fdcd7602d9d482894d56e42ac14c0
Adam Wilson <adamwilson1234@gmail.com>
2016-11-02 19:33:38 -0500

    saved data object into meta and cars $scope variables. cars array data rendering with ng-repeat into page as table rows.

--- app.js
+++ app.js
@@ -11,9 +11,20 @@
     app.controller('CarController', ['$http', '$scope',
         function($http, $scope){
             // get data and put on a scope var avail to html
-            $http.get('relocated.json').success(function(data){
-                $scope.cars = data;
-                console.log(data);
+            $http.get('relocated.json').success(function(dataObject){
+                // just the meta data, description etc.
+                $scope.meta = dataObject.meta;
+                console.log($scope.meta);
+
+                // just array of car moves
+                $scope.cars = dataObject.data;
+                console.log($scope.cars);
+                // just color of first moved car
+                console.log($scope.cars[0][10]);
+
+
+
+
             });
 
         }]); // end CarController
