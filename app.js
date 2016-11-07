8d0f460084d2e2db8d917f6038d75544ff55721d
Adam Wilson <adamwilson1234@gmail.com>
2016-11-07 17:02:43 -0600

    wrote working date range filter with type=date inputs but was super buggy so commented it out for the moment. issues included: filter running toISOString and other code on date inputs but trouble with default values and filter running prior to user input. Also trouble with filter running operations on JSON values before json has loaded.

--- app.js
+++ app.js
@@ -30,29 +30,40 @@
     // 1. all data (array of arrays), 2. start date, 3. end date
     // returns array of arrays whose date value passes conditionals
     app.filter('dateRangeFilter', function(){
-        return function(values, inputDate){
+        return function(values, dateFrom, dateTo){
+
+            // wait 200ms for controller to get data then execute filter code
+            // window.setTimeout(filterValues(values, dateFrom, dateTo), 200);
+
             // see if date field on each item is >= from and <= to
             // then push onto array if passes and return array at end
             console.log('hello from dateRangeFilter');
             console.log(values);
-            console.log(inputDate);
-            // var dateSubString = values[0][8].substr(0, 10);
-            // console.log(dateSubString);
+            console.log(dateFrom);
+            console.log( typeof dateFrom);
+
+            console.log(dateTo);
+            var sampleDateString = values[0][8].substr(0, 10);
+            console.log(sampleDateString);
             // console.log(items);
             // console.log(dateInput);
             // return values;
             var filteredData = [];
+            var fromString = dateFrom.toISOString().split('T')[0];
+            var toString = dateTo.toISOString().split('T')[0];
+            console.log(fromString);
             for (var i=0; i<values.length; i++){
                 var dateSubString = values[i][8].substr(0, 10);
-                if (dateSubString >= inputDate){
+                if (dateSubString >= fromString && dateSubString <= toString){
                     filteredData.push(values[i]);
                 }
             }
             return filteredData;
 
-
         };
     });
+
+    
 
 
 })();
